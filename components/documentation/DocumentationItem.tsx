2be68522780fe5f3df132edeb7f7c80b10d44581
ThomasBelrayDev <75146145+H4rasho@users.noreply.github.com>
2022-09-16 21:10:06 -0300

    change a to button with copy text to clipboard

--- components/documentation/DocumentationItem.tsx
+++ components/documentation/DocumentationItem.tsx
@@ -1,29 +1,35 @@
-import { Badge, Code, Flex } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Flex, useToast } from "@chakra-ui/react";
 
 export default function DocumentationItem({
   description,
   endpoint,
 }: {
-    description: React.ReactNode;
+  description: React.ReactNode;
   endpoint: string;
 }) {
+  const toast = useToast();
   return (
     <>
       {description}
-      <a href={endpoint} target="_black" rel="noreferrer noopener">
-        <Flex gap={2}>
-          <Badge>GET</Badge>
-          <Code
-            _hover={{
-              backgroundColor: "purple.500",
-              color: "white",
-              transition: "all 0.6s",
-            }}
-          >
-            {endpoint}{" "}
-          </Code>
-        </Flex>
-      </a>
+      <Flex gap={2}>
+        <Button
+          _hover={{
+            backgroundColor: "purple.500",
+            color: "white",
+            transition: "all 0.6s",
+          }}
+          onClick={() => {
+            navigator.clipboard.writeText(endpoint);
+            toast({
+              title: "Copiado en el portapapeles.",
+              status: "success",
+              duration: 1000,
+            });
+          }}
+        >
+          {endpoint}{" "}
+        </Button>
+      </Flex>
     </>
   );
 }
