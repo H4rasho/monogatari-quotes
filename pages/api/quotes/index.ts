e90ab648daddfc3be1f47a04c9c2a0688eff861a
ThomasBelrayDev <75146145+H4rasho@users.noreply.github.com>
2022-10-13 01:45:40 +0100

    fix limit pagination

--- pages/api/quotes/index.ts
+++ pages/api/quotes/index.ts
@@ -7,7 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Quote[] | unknown>
 ) {
-  const limit = req.query.limit ?? Infinity;
+  const limit = req.query.limit ?? 1000;
   const offset = req.query.offset ?? 0;
   const { results, error } = await getQuotes(Number(offset), Number(limit));
   if (results) return res.json(results);
