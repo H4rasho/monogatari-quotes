6f597b818eb7376dbf7a54263e90ea17e2f1a6f5
h4rasho <h4rasho@gmail.com>
2023-04-26 00:19:41 -0400

    add images in public dir

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { Button, HStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { ArrowBackIcon, ArrowForwardIcon } from "@chakra-ui/icons";
+import Image from "next/image";
 
 interface Props {
   quotes: Quote[];
@@ -29,7 +30,11 @@ const Home = ({ quotes }: Props) => {
         <meta name="description" content="List of Monogatari Series quotes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Image
+        src={"/../public/images/nadeko-sengoku.jpg"}
+        width="33"
+        height="31"
+      />
       <Layout>
         <H1 mt={10}>Monogatari Quotes</H1>
         <QuotesList quotes={quotes}></QuotesList>
